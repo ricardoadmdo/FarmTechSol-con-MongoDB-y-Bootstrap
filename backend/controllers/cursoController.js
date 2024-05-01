338742ec3bc42f84d3b6b293f21d1d12d7760d66
ricardoadmdo <descoubet0107@gmail.com>
2024-05-01 01:32:53 -0400

    aa

--- backend/controllers/cursoController.js
+++ backend/controllers/cursoController.js
@@ -1,103 +1,103 @@
-const { response } = require("express");
-const Curso = require("../models/Curso.js");
+const { response } = require('express');
+const Curso = require('../models/Curso.js');
 
 const getCursos = async (req, res = response) => {
-    const cursos = await Curso.find().populate("nombre");
-    res.json({
-        ok: true,
-        cursos,
-    });
+	const cursos = await Curso.find().populate('nombre');
+	res.json({
+		ok: true,
+		cursos,
+	});
 };
 
 const createCurso = async (req, res = response) => {
-    const { nombre, precio, description } = req.body;
+	const { nombre, precio, description } = req.body;
 
-    try {
-        const curso = new Curso({
-            nombre,
-            precio,
-            description,
-        });
+	try {
+		const curso = new Curso({
+			nombre,
+			precio,
+			description,
+		});
 
-        const cursoGuardado = await curso.save();
-        res.json({
-            ok: true,
-            curso: cursoGuardado,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: "Hable con el admin",
-        });
-    }
+		const cursoGuardado = await curso.save();
+		res.json({
+			ok: true,
+			curso: cursoGuardado,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Hable $con el admin',
+		});
+	}
 };
 const updateCurso = async (req, res = response) => {
-    const { id: cursoId, nombre, description, precio } = req.body;
-    try {
-        const curso = await Curso.findById(cursoId);
-        if (!curso) {
-            return res.status(400).json({
-                ok: false,
-                msg: "Curso no existe por ese id",
-            });
-        }
+	const { id: cursoId, nombre, description, precio } = req.body;
+	try {
+		const curso = await Curso.findById(cursoId);
+		if (!curso) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Curso no existe por ese id',
+			});
+		}
 
-        const nuevoCurso = {
-            nombre,
-            description,
-            precio,
-        };
+		const nuevoCurso = {
+			nombre,
+			description,
+			precio,
+		};
 
-        const cursoActualizado = await Curso.findByIdAndUpdate(
-            cursoId,
-            nuevoCurso,
-            { new: true }
-        );
+		const cursoActualizado = await Curso.findByIdAndUpdate(
+			cursoId,
+			nuevoCurso,
+			{ new: true }
+		);
 
-        res.json({
-            ok: true,
-            curso: cursoActualizado,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: "Hable con el administrador",
-        });
-    }
+		res.json({
+			ok: true,
+			curso: cursoActualizado,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Hable con el administrador',
+		});
+	}
 };
 
 const deleteCurso = async (req, res = response) => {
-    const cursoId = req.params.id;
+	const cursoId = req.params.id;
 
-    try {
-        const curso = await Curso.findById(cursoId);
+	try {
+		const curso = await Curso.findById(cursoId);
 
-        if (!curso) {
-            return res.status(400).json({
-                ok: false,
-                msg: "No existe un curso con ese id",
-            });
-        }
+		if (!curso) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'No existe un curso con ese id',
+			});
+		}
 
-        await Curso.findByIdAndDelete(cursoId);
+		await Curso.findByIdAndDelete(cursoId);
 
-        res.json({
-            ok: true,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: "Hable con el administrador",
-        });
-    }
+		res.json({
+			ok: true,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Hable con el administrador',
+		});
+	}
 };
 
 module.exports = {
-    getCursos,
-    createCurso,
-    updateCurso,
-    deleteCurso,
+	getCursos,
+	createCurso,
+	updateCurso,
+	deleteCurso,
 };
